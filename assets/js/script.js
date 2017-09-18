9e7c839fcff58d5e1b28bc8a4f0f1be78b521932
Sergei <pryadko.sergei@gmail.com>
2017-09-18 16:17:45 +0300

    'add_refinement'

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,14 @@ $(function () {
         var currentImageIndex = $('.img.first').index();
         var nextImageIndex = currentImageIndex +1;
         var nextImage = $('.img').eq(nextImageIndex);
+        currentImage.fadeOut(1000);
         currentImage.removeClass('first');
 
         if (nextImageIndex === ($('.img:last').index()+1)) {
+            $('.img').eq(0).fadeIn(1000);
            $('.img').eq(0).addClass('first');
         }else {
+            nextImage.fadeIn(1000);
             nextImage.addClass('first');
         }
     });
@@ -20,7 +23,9 @@ $(function () {
         var prevImageIndex = currentImageIndex -1;
         var prevImage = $('.img').eq(prevImageIndex);
 
+        currentImage.fadeOut(1000);
         currentImage.removeClass('first');
+        prevImage.fadeIn(1000);
         prevImage.addClass('first');
 
     });
